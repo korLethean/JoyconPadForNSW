bfa008c1d7fc159f66d442985f77d81f04495bc5
Lethean <hdgwon@naver.com>
2019-04-29 17:52:13 +0900

    socket transmission for client device

--- app/src/main/java/com/example/hd/joyconpadfornsw/MainActivity.java
+++ app/src/main/java/com/example/hd/joyconpadfornsw/MainActivity.java
@@ -4,11 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import android.os.Handler;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String IP = "192.168.0.1";
+
+    private static final int PORT = 5000;
+    private Socket socket;
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+    private Handler handler;
+    private String writeMsg, readMsg;
 
     @BindView(R.id.button_up)
     Button btnUp;
@@ -34,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
     Button btnZ;
     @BindView(R.id.button_zl)
     Button btnZl;
+    @BindView(R.id.button_connect)
+    Button btnConnect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +59,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.button_connect})
+    void connectClickEvent(Button button) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket = new Socket(InetAddress.getByName(IP), PORT);
+                    inputStream = new DataInputStream(socket.getInputStream());
+                    outputStream = new DataOutputStream(socket.getOutputStream());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                while (true) {
+                    try {
+                        readMsg = inputStream.readUTF();
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                            }
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
     @OnClick({R.id.button_up, R.id.button_down, R.id.button_left, R.id.button_right, R.id.button_a, R.id.button_b, R.id.button_x, R.id.button_y, R.id.button_sl,
